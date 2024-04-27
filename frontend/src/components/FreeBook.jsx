8a1386df28b9200a923035436ecf332de9309327
Munna kumar <munnakumarjha@gmail.com>
2024-04-27 20:42:40 +0530

    Frontend is Done

--- frontend/src/components/FreeBook.jsx
+++ frontend/src/components/FreeBook.jsx
@@ -1,76 +1,29 @@
-// // import React from 'react'
-// import list from '../../public/list.json'
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import Slider from "react-slick";
-// import Cards from './Cards';
-// function FreeBook() {
-//     var settings = {
-//         dots: true,
-//         infinite: false,
-//         speed: 500,
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//         initialSlide: 0,
-//         responsive: [
-//           {
-//             breakpoint: 1024,
-//             settings: {
-//               slidesToShow: 3,
-//               slidesToScroll: 3,
-//               infinite: true,
-//               dots: true
-//             }
-//           },
-//           {
-//             breakpoint: 600,
-//             settings: {
-//               slidesToShow: 2,
-//               slidesToScroll: 2,
-//               initialSlide: 2
-//             }
-//           },
-//           {
-//             breakpoint: 480,
-//             settings: {
-//               slidesToShow: 1,
-//               slidesToScroll: 1
-//             }
-//           }
-//         ]
-//       };
+import  { useEffect, useState } from "react";
 
-//     const filterData=list.filter((data)=>data.category==='Free')
-//     console.log(filterData)
-//   return (
-//     <>
-//     <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
-//       <div>
-//       <h1 className='font-semibold text-xl pb-2 mt-8'>Free Offered Course</h1>
-//       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula lectus eget efficitur maximus.</p>
-//       </div>
-    
-//     <div>
-//     <Slider {...settings}>
-//         {filterData.map((item)=>{
-//             <Cards item={item} key={item.id}/>
-//         })}
-//       </Slider>
-//     </div>
-//     </div>
-//     </>
-//   )
-// }
-
-// export default FreeBook
-
-import list from '../../public/list.json';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import Cards from './Cards';
 
-function FreeBook() {
+import axios from "axios";
+
+import Cards from "./Cards";
+function Freebook() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+
+        const data = res.data.filter((data) => data.category === "Free");
+        console.log(data);
+        setBook(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+
   var settings = {
     dots: true,
     infinite: false,
@@ -85,45 +38,47 @@ function FreeBook() {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
-
-  const filterData = list.filter((data) => data.category === 'Free');
-
   return (
-    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
-      <div>
-        <h1 className='font-semibold text-xl pb-2 mt-8'>Free Offered Course</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula lectus eget efficitur maximus.</p>
-      </div>
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Accusantium veritatis alias pariatur ad dolor repudiandae eligendi
+            corporis nulla non suscipit, iure neque earum?
+          </p>
+        </div>
 
-      <div>
-        <Slider {...settings}>
-          {filterData.map((item) => (
-            <Cards item={item} key={item.id} />
-          ))}
-        </Slider>
+        <div>
+          <Slider {...settings}>
+            {book.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+          </Slider>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
-
-export default FreeBook;
+export default Freebook;
